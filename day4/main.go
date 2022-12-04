021b571cdda4f29e48ed2bd7746b2c5af8c1cbc0
Donkie <daniel.cf.hultgren@gmail.com>
2022-12-04 21:57:29 +0100

    Day 4 - part 2

--- day4/main.go
+++ day4/main.go
@@ -9,20 +9,37 @@ import (
 	"strings"
 )
 
+// Represents a range of numbers
 type Range struct {
 	min int
 	max int
 }
 
+// Represents a pair of Ranges from the input data
 type RangePair struct {
 	r1 Range
 	r2 Range
 }
 
-func (r Range) FullyContains(r2 Range) bool {
-	return r2.min >= r.min && r2.max <= r.max
+// Returns true if this range contains the supplied number
+func (r Range) Contains(num int) bool {
+	return num >= r.min && num <= r.max
 }
 
+// Returns true if this range fully contains the other range
+func (r Range) FullyContains(r2 Range) bool {
+	return r.Contains(r2.min) && r.Contains(r2.max)
+}
+
+// Returns true if this range overlaps any part of the supplied range
+func (r Range) Overlaps(r2 Range) bool {
+	return r.Contains(r2.min) || r.Contains(r2.max)
+}
+
+// Returns a new Range object parsed from the input string
+// Returns an error if the input string has an invalid format.
+// Valid format of the input string is "A-B", where A and B are ints.
+// Example "123-456"
 func rangeFromString(str string) (*Range, error) {
 	strs := strings.Split(str, "-")
 	if len(strs) != 2 {
@@ -42,6 +59,9 @@ func rangeFromString(str string) (*Range, error) {
 	return r, nil
 }
 
+// Parses the input file
+// Returns a list of all pairs of ranges in the input
+// Exits the program if the format of one of the lines is invalid
 func parseInput(fileName string) []RangePair {
 	file, err := os.Open(fileName)
 	if err != nil {
@@ -83,10 +103,22 @@ func parseInput(fileName string) []RangePair {
 	return pairs
 }
 
+// Returns the number of pairs in the list where one of the ranges fully contain the other
 func getNumPairsFullyContaining(pairs []RangePair) int {
 	sum := 0
 	for _, pair := range pairs {
 		if pair.r1.FullyContains(pair.r2) || pair.r2.FullyContains(pair.r1) {
+			sum++
+		}
+	}
+	return sum
+}
+
+// Returns the number of pairs in the list where the ranges overlap each other
+func getNumPairsOverlapping(pairs []RangePair) int {
+	sum := 0
+	for _, pair := range pairs {
+		if pair.r1.Overlaps(pair.r2) || pair.r2.Overlaps(pair.r1) {
 			sum++
 		}
 	}
@@ -98,4 +130,7 @@ func main() {
 
 	numPairFullyContaining := getNumPairsFullyContaining(pairs)
 	fmt.Printf("Part 1 - Number of pairs that fully contain the other: %d\n", numPairFullyContaining)
+
+	numPairOverlapping := getNumPairsOverlapping(pairs)
+	fmt.Printf("Part 2 - Number of pairs that overlaps the other: %d\n", numPairOverlapping)
 }
